fe1329028966ab2044c68e531417cbef20196ee7
TypicallyThomas <thomasbunink@gmail.com>
2024-01-10 09:46:25 +0100

    Simplified button presses, removed repetetive code

--- Firmware/code.py
+++ Firmware/code.py
@@ -5,7 +5,7 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-#Setting pin varables
+# Setting pin varables
 Screen_1_Btn_Pin = board.GP1
 Screen_2_Btn_Pin = board.GP2
 Screen_3_Btn_Pin = board.GP3
@@ -13,30 +13,30 @@ Screen_4_Btn_Pin = board.GP4
 Censor_Btn_Pin = board.GP15
 
 
-#..
+# ..
 keyboard = Keyboard(usb_hid.devices)
 
-#Screen Button 1
+# Screen Button 1
 Screen_1_Btn = digitalio.DigitalInOut(Screen_1_Btn_Pin)
 Screen_1_Btn.direction = digitalio.Direction.INPUT
 Screen_1_Btn.pull = digitalio.Pull.DOWN
 
-#Screen Button 2
+# Screen Button 2
 Screen_2_Btn = digitalio.DigitalInOut(Screen_2_Btn_Pin)
 Screen_2_Btn.direction = digitalio.Direction.INPUT
 Screen_2_Btn.pull = digitalio.Pull.DOWN
 
-#Screen Button 3
+# Screen Button 3
 Screen_3_Btn = digitalio.DigitalInOut(Screen_3_Btn_Pin)
 Screen_3_Btn.direction = digitalio.Direction.INPUT
 Screen_3_Btn.pull = digitalio.Pull.DOWN
 
-#Screen Button 4
+# Screen Button 4
 Screen_4_Btn = digitalio.DigitalInOut(Screen_4_Btn_Pin)
 Screen_4_Btn.direction = digitalio.Direction.INPUT
 Screen_4_Btn.pull = digitalio.Pull.DOWN
 
-#Censor Button
+# Censor Button
 Censor_Btn = digitalio.DigitalInOut(Censor_Btn_Pin)
 Censor_Btn.direction = digitalio.Direction.INPUT
 Censor_Btn.pull = digitalio.Pull.DOWN
@@ -48,29 +48,29 @@ while True:
         keyboard.press(Keycode.SPACEBAR)
         time.sleep(0.1)
         keyboard.release(Keycode.SPACEBAR)
-        
+
     if Screen_1_Btn.value:
         print("Buttion 1 pressed")
-        keyboard.press(Keycode.ONE)    
+        keyboard.press(Keycode.ONE)
         time.sleep(0.1)
         keyboard.release(Keycode.ONE)
-        
+
     if Screen_2_Btn.value:
         print("Buttion 2 pressed")
-        keyboard.press(Keycode.TWO)    
+        keyboard.press(Keycode.TWO)
         time.sleep(0.1)
         keyboard.release(Keycode.TWO)
-    
+
     if Screen_3_Btn.value:
         print("Buttion 3 pressed")
-        keyboard.press(Keycode.THREE)    
+        keyboard.press(Keycode.THREE)
         time.sleep(0.1)
         keyboard.release(Keycode.THREE)
-        
+
     if Screen_4_Btn.value:
         print("Buttion 3 pressed")
-        keyboard.press(Keycode.FOUR)    
+        keyboard.press(Keycode.FOUR)
         time.sleep(0.1)
         keyboard.release(Keycode.FOUR)
-        
+
     time.sleep(0.1)
